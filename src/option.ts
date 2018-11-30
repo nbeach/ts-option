661cd74f9adebc8b756fe7d424ff5b5e760fa23a
nbeach <nicholascbeach@gmail.com>
2018-11-29 19:54:15 -0500

    Fixed Array inheritance for all but one method

--- src/option.ts
+++ src/option.ts
@@ -5,63 +5,64 @@ export interface Option<A> extends Array<A> {
     filter(callback: (value: A, index: number, array: A[]) => any, thisArg?: any): Option<A>
     orElse<B>(alternative: Option<B>): Option<A | B>
     getOrElse<B>(defaultValue: B): A | B
-    get(): A
-    orNull(): A | null
-    nonEmpty(): boolean
-    isEmpty(): boolean
+    readonly get: A
+    readonly orNull: A | null
+    readonly nonEmpty: boolean
+    readonly isEmpty: boolean
 }
 
-class OptionInstance<A> extends Array<A> implements Option<A> {
+class OptionInstance<A> extends Array<A> implements Option<A>  {
     constructor(value: A | undefined | null) {
         super()
+        Object.setPrototypeOf(this, Object.create(OptionInstance.prototype))
         if (value !== undefined && value !== null) {
             this[0] = value
         }
-        Object.freeze(this)
     }
 
-    public map = <B>(callback: (value: A, index: number, array: A[]) => B, thisArg?: any): Option<B> => {
-        return new OptionInstance(super.map(callback, thisArg)[0])
+    public map<B>(callback: (value: A, index: number, array: A[]) => B, thisArg: any = this): Option<B> {
+        return this.isEmpty ? None : new OptionInstance(Array.prototype.map.call(this, callback, thisArg)[0])
     }
 
-    public flatMap = <B>(callback: (value: A) => Option<B>): Option<B> => {
+    public flatMap<B>(callback: (value: A) => Option<B>): Option<B> {
         return this.map(callback).flatten()
     }
 
-    public flatten = <T>(): Option<T> => {
-        return this.nonEmpty() ? this.get() as any : None
+    public flatten<T>(): Option<T> {
+        return this.nonEmpty ? this.get as any : None
     }
 
-    public filter = (callback: (value: A, index: number, array: A[]) => any, thisArg?: any): Option<A> => {
-        return new OptionInstance(super.filter(callback, thisArg)[0])
+    public filter(callback: (value: A, index: number, array: A[]) => any, thisArg: any = this): Option<A> {
+        return new OptionInstance(Array.prototype.filter.call(this, callback, thisArg)[0])
     }
 
-    public orElse = <B>(alternative: Option<B>): Option<A | B> => {
-        return this.nonEmpty() ? this : alternative
+    public orElse<B>(alternative: Option<B>): Option<A | B> {
+        return this.nonEmpty ? this : alternative
     }
 
-    public getOrElse = <B>(defaultValue: B): A | B => {
-        return this.nonEmpty() ? this.get() : defaultValue
+    public getOrElse<B>(defaultValue: B): A | B {
+        return this.nonEmpty ? this.get : defaultValue
     }
 
-    public get = (): A => {
-        if (this.isEmpty()) { throw new Error("Value not present") }
+    get get(): A {
+        if (this.isEmpty) { throw new Error("Value not present") }
         return this[0]
     }
 
-    public orNull = (): A | null => {
+    get orNull(): A | null {
         return this.getOrElse(null)
     }
 
-    public nonEmpty = (): boolean => {
-        return !this.isEmpty()
+    get nonEmpty(): boolean {
+        return !this.isEmpty
     }
 
-    public isEmpty = (): boolean => {
+    get isEmpty(): boolean {
         return this.length === 0
     }
 
 }
 
-export const Some = <T>(val: T) => new OptionInstance(val)
-export const None = new OptionInstance(null) as Option<any>
+
+export const Some = <T>(val: T) => Object.freeze(new OptionInstance(val)) as Option<T>
+export const None = Object.freeze(new OptionInstance(null) ) as Option<any>
